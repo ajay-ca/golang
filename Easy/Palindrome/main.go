3320d8be761a1e9b7917f3714895f562d3667f22
ajay-ca <ajayappu6@gmail.com>
2023-05-09 10:07:03 +0530

    Updated comments

--- Easy/Palindrome/main.go
+++ Easy/Palindrome/main.go
@@ -4,8 +4,21 @@ import "fmt"
 
 func main (){
 
-	var num int
+	var num,temp,revnum int
 	fmt.Println("Enter your number:")
-	fmt.Scanf("%d",num)
+	fmt.Scanln(&num)
+
+	temp = num
+	revnum = 0
+	for temp > 0 {
+		revnum*=10			//increasing the digits place
+		revnum+=temp%10		//adding the last digit to reverse
+		temp/=10			//removing the last digit
+	}
+	if num == revnum {
+		fmt.Println("The number is a palindrome")
+	} else {
+		fmt.Println("The number is not a palindrome")
+	}
 
 }
