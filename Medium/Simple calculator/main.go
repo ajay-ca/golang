5796441ab82b73c57d6764c9441d09f9b9c865ee
ajay-ca <ajayappu6@gmail.com>
2023-05-18 17:30:57 +0530

    Working commit

--- Medium/Simple calculator/main.go
+++ Medium/Simple calculator/main.go
@@ -1,16 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var a,b,option int
-	fmt.Print("Enter the two numbers:")
-	fmt.Print("a = ")
-	fmt.Scanln("%d",&a)
+	var a, b, option, result float64
+	fmt.Print("Enter the two numbers:\na = ")
+	fmt.Scan(&a)
 	fmt.Print("b = ")
-	fmt.Scanln("%d",&b)
-	fmt.Print("Select the operation:\n 1. Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponent\nEnter your option: ")
-	fmt.Scanln("%d",&option)
-	
-
+	fmt.Scan(&b)
+	fmt.Print("Select the operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exponent\nEnter your option: ")
+calculation:
+	fmt.Scan(&option)
+	switch option {
+	case 1:
+		result = a + b
+	case 2:
+		result = a - b
+	case 3:
+		result = a * b
+	case 4:
+		result = a / b
+	case 5:
+		result = math.Pow(a, b)
+	default:
+		fmt.Println("Invalid option. Try again: ")
+		goto calculation
+	}
+	fmt.Printf("The result is %.2f\n", result)
 }
